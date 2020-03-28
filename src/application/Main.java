2dab57d41a2452cdef61027d46ed6eb545dc59e5
Duduxs <duduxss3@gmail.com>
2020-03-28 00:46:26 -0300

    About view implemented

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,8 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -20,7 +22,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			// Set stage and title at Stage in arguments, after show the screen.
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
@@ -30,6 +32,10 @@ public class Main extends Application {
 		}
 	}
 
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
@@ -15,26 +24,47 @@ public class MainViewController implements Initializable {
 	private MenuItem menuItemDepartment;
 	@FXML
 	private MenuItem menuItemAbout;
-	
+
 	@FXML
 	public void onMenuItemSellerAction() {
 		System.out.println("onMenuItemSellerAction!");
 	}
-	
+
 	@FXML
 	public void onMenuItemDepartmentSellerAction() {
 		System.out.println("onMenuDepartmentSellerAction!");
 	}
-	
+
 	@FXML
 	public void onMenuItemAboutSellerAciton() {
 		System.out.println("onMenuAboutSellerAction!");
+		loadView("/gui/About.fxml");
 	}
 
-	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
+	}
+
+	// Load the About screen, the Synchronized = the code will be not interrupted
+	private synchronized void loadView(String absoluteName) {
+		try {
+			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", null, "Error loading view", AlertType.ERROR);
+		}
 	}
 
 }
