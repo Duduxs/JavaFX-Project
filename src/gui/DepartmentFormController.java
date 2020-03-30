eb64ed5829eae5b86824518ee83fa6d65f14a344
Duduxs <duduxss3@gmail.com>
2020-03-30 14:43:24 -0300

    Observer pattern and att date implemented

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,19 +1,22 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
 import model.services.DepartmentService;
 
@@ -23,6 +26,9 @@ public class DepartmentFormController implements Initializable {
 
 	private DepartmentService service;
 
+	//List that save a objects for event atualization
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+	
 	@FXML
 	private TextField txtId;
 	@FXML
@@ -44,27 +50,35 @@ public class DepartmentFormController implements Initializable {
 		this.service = service;
 	}
 	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+
 	@FXML
 	public void onBtnSaveAction(ActionEvent event) {
-		if(entity == null) 
+		if (entity == null)
 			throw new IllegalStateException("Entity was null");
-	
-		if (service == null) 
+
+		if (service == null)
 			throw new IllegalStateException("Service was null");
-		
+
 		try {
 			entity = getFormData();
 			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();
 			Utils.currentStage(event).close();
-		}
-		catch(DbException e) {
+		} catch (DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
 		}
 	}
-	
 
-	
-	
+	private void notifyDataChangeListeners() {
+		for(DataChangeListener listener: dataChangeListeners) {
+			listener.onDataChanged();
+		}
+		
+	}
+
 	private Department getFormData() {
 		Department obj = new Department();
 
@@ -93,7 +107,5 @@ public class DepartmentFormController implements Initializable {
 		txtName.setText(entity.getName());
 
 	}
-
-
 
 }
